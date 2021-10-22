d5ee47121c8e0197277e1e9361616a68a30223c3
Karim <kimou007@live.fr>
2021-10-22 17:32:01 +0200

    ubung Liniendiagramm

--- 1-05-Liniendiagramm-Uebung/Liniendiagramm-Uebung.py
+++ 1-05-Liniendiagramm-Uebung/Liniendiagramm-Uebung.py
@@ -9,7 +9,6 @@ import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
-
 # Erzeuge ein Pandas DataFrame aus 2010YumaAZ.csv
 df = pd.read_csv('../DATA/2010YumaAZ.csv')
 days = ['TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','SUNDAY','MONDAY']
@@ -22,24 +21,25 @@ data = []
 for day in days:
     # Was gehört in diesen Aufruf von Scatter?
     trace = go.Scatter(
-            x = df[df["DAY"] == day]["LST_TIME"],
-            y = df[df["DAY"] == day]["T_HR_AVG"],
-            name = day,
-            mode = "markers+lines"
+            x = df[df["DAY"]==day]["LST_TIME"],
+            y = df[df["DAY"]==day]["T_HR_AVG"],
+            mode = "lines",
+            name = day
+            
         )
     data.append(trace)
 
 # Definiere das Layout
 
 layout = go.Layout(
-        title = "Temperaturschwankungen",
-        xaxis = {"title": "Zeit in hh:mm"},
-        yaxis = {"title": "Temperatur in °C"},
-        hovermode = "closest")
+        title = "Temperaturaufzeichnungen von sieben Tagen",
+        xaxis = dict(title="Time in hh:mm"),
+        yaxis = dict(title="Temperatur in °C"),
+        hovermode = "closest"
+    )
 
 
 # Erzeuge eine fig mit den Daten und dem Layout und plotte die fig
-fig = go.Figure(data = data, layout = layout)
+fig = go.Figure(data=data, layout = layout)
 pyo.plot(fig)
-
 
