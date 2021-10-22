43e78ebddefc616402356a5a30abd3ddc2fb5c8c
Karim <kimou007@live.fr>
2021-10-22 22:53:52 +0200

    dash styling

--- 2-01-DashLayout/test.py
+++ 2-01-DashLayout/test.py
@@ -4,8 +4,16 @@ import dash_html_components as html
 
 app = dash.Dash()
 
+colors = {"background": "#111111", "text": "#50DB44"}
+
+
 app.layout = html.Div(children=[
-    html.H1(children="Hello Dash"),
+    html.H1(children="Hello Dash",
+            style={
+                    "textAlign": "center",
+                    "color": colors["text"]
+            }
+            ),
     html.H2(children="Ahla b zebi"),
     html.P(children="everybody wanna live together"),
     html.Div(children="Dashhhhhh"),
@@ -28,11 +36,18 @@ app.layout = html.Div(children=[
                     }
 
             ],
-            "layout": {"title": "Dash Viz"}
+            "layout": {
+                        "title": "Dash Viz",
+                        "plot_bgcolor": colors["background"],
+                        "paper_bgcolor": colors["background"],
+                        "font": {"color": colors["text"]}
+                      }
 
         }
     )
-] )
+    ],
+    style={"backgroundColor": "#ffffff"}
+    )
 
 if __name__ == "__main__":
     app.run_server()
